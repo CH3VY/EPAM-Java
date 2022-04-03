bed32dc7ce162aa00ec9a4d4e8b4de4f20d363f7
CHEVY <aidyn.seipolla@nu.edu.kz>
2022-04-03 19:30:52 +0600

    renamed class name

--- src/JavaClasses/classesMain1.java
+++ src/JavaClasses/classesMain1.java
@@ -2,7 +2,7 @@ package JavaClasses;
 // EPAM Automated Testing: Java Foundations
 // Aidyn Seipolla
 
-public class Classes1 {
+public class classesMain1 {
     public static void main(String args[]) {
         System.out.println("Fefesfes");
     }

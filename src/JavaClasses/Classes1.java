c3b918b154ed59ce83a63b3baf8087c91cbe2aa1
CHEVY <aidyn.seipolla@nu.edu.kz>
2022-04-03 19:28:57 +0600

    separated tasks into packages

---
+++ src/JavaClasses/Classes1.java
@@ -0,0 +1,9 @@
+package JavaClasses;
+// EPAM Automated Testing: Java Foundations
+// Aidyn Seipolla
+
+public class Classes1 {
+    public static void main(String args[]) {
+        System.out.println("Fefesfes");
+    }
+}
